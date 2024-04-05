91389a4015ab0b8ea0a65ba1e27d17567c7ec04f
Pikrpan <54728861+Pikrpan@users.noreply.github.com>
2024-04-05 11:54:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//Přepiš text HTML elementu footer na své jméno
+
+
+
+//Vytvoř funkci s názvem zmenJmenaHracu
+//Funkce zmenJmenaHracu změní text v odstavcích s id pPlayer1 a pPlayer2 na nově zadaný text
+
+
+
+
+//Vytvoř funkci s názvem hodKostkami
+/*
+Tato funkce bude obsahovat minimálně:
+  - dvě proměnné, které vygenerují náhodné číslo od jedné do šesti (pro každého hráče zvlášť),
+  - příkaz, který změní obrázek kostky podle hozeného čísla,
+  - blok kódu, který porovná který hráč vyhrál a zapíše výsledek do odstavce s id "pResult"
+
+HODNĚ ŠTĚSTÍ!! 
+PS: Představivosti se meze nekladou.
+*/
+
+
